860347066d025a61d59e0383ae6febcf999a6a5d
Mohammad <mohammad@britecore.com>
2019-04-04 23:15:18 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="document-your-code",
-    version="0.0.7",
+    version="0.0.8",
     author="Mohammad Albakri",
     author_email="mohammad.albakri93@gmail.com",
     packages=find_packages(),
