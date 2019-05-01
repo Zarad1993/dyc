32add98b2b29b2b05eb64e822dcd32a0a84ebe0f
vccolombo <victorcora98@gmail.com>
2019-05-01 10:59:13 -0300

    Change version in setup.py for release of issue #8 fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="document-your-code",
-    version="0.1.3",
+    version="0.1.4",
     author="Mohammad Albakri",
     author_email="mohammad.albakri93@gmail.com",
     packages=find_packages(),
