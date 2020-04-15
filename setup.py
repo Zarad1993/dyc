10fc55e212de7bfd6fb8fcc10a4be8043be23d30
Mohammad <mohammad@britecore.com>
2020-04-15 15:05:26 +0300

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="document-your-code",
-    version="0.2.2",
+    version="0.2.3",
     author="Mohammad Albakri",
     author_email="mohammad.albakri93@gmail.com",
     packages=find_packages(),
